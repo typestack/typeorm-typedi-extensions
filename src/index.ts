1002d8e07879fb63ad4efcd5b483f544fa9dc15b
Michał Lytek <michal.ly@wp.pl>
2018-02-23 16:03:58 +0100

    Add `Orm` aliases for backward compatibility

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
 export * from "./decorators/InjectConnection";
 export * from "./decorators/InjectManager";
 export * from "./decorators/InjectRepository";
+
+// deprecated aliases
+export { InjectConnection as OrmConnection } from "./decorators/InjectConnection";
+export { InjectManager as OrmManager } from "./decorators/InjectManager";
+export { InjectRepository as OrmRepository } from "./decorators/InjectRepository";
