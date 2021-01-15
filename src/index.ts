bedaca7d8610598f8a5e5b8edaddbb63b566b640
Attila Olah <NoNameProvided@users.noreply.github.com>
2021-01-15 20:10:40 +0100

    refactor: remove deprecated aliases

--- src/index.ts
+++ src/index.ts
@@ -10,8 +10,3 @@ Container.set({ id: ConnectionManager, type: ConnectionManager });
 export * from './decorators/InjectConnection';
 export * from './decorators/InjectManager';
 export * from './decorators/InjectRepository';
-
-// deprecated aliases
-export { InjectConnection as OrmConnection } from './decorators/InjectConnection';
-export { InjectManager as OrmManager } from './decorators/InjectManager';
-export { InjectRepository as OrmRepository } from './decorators/InjectRepository';
