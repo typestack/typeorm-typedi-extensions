abc4b3a08eda9b9bc81758dfcf3213ed08af0d9f
Attila Olah <NoNameProvided@users.noreply.github.com>
2021-01-15 19:54:06 +0100

    fix: make lib compatible with TypeDI 0.9.0+

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
+import { Container } from 'typedi';
+import { ConnectionManager } from 'typeorm';
+
+/** 
+ * We need to set imported TypeORM classes before requesting them, otherwise we
+ * would receive a "ServiceNotFoundError" above TypeDI 0.9.1.
+ */
+Container.set({ id: ConnectionManager, type: ConnectionManager });
+
 export * from './decorators/InjectConnection';
 export * from './decorators/InjectManager';
 export * from './decorators/InjectRepository';
