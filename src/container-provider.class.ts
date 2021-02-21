73d9e063309aa3cb16746cf3c33f6a25e525fc0f
Attila Olah <NoNameProvided@users.noreply.github.com>
2021-02-22 00:55:49 +0100

    feat: add custom Continer provider

---
+++ src/container-provider.class.ts
@@ -0,0 +1,19 @@
+import { ContainedType, ContainerInterface,  } from 'typeorm';
+import { Container, Constructable } from 'typedi';
+
+/**
+ * Class transforming between TypeDI API and the expected API by TypeORM.
+ */
+export class TypeDIContainerProvider implements ContainerInterface {
+  get<T>(constructable: ContainedType<T>) {
+    /** 
+     * TypeDI only resolves values for registered types, so we need to register
+     * them before to requesting them from the default container.
+     */
+    if (!Container.has(constructable as Constructable<T>)) {
+      Container.set({ id: constructable, type: constructable as Constructable<T> });
+    }
+
+    return Container.get(constructable as Constructable<T>);
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
-import { Container } from 'typedi';
+import { Container as TypeDIContainer } from 'typedi';
 import { ConnectionManager } from 'typeorm';
-
-/**
- * We need to set imported TypeORM classes before requesting them, otherwise we
- * would receive a "ServiceNotFoundError" above TypeDI 0.9.1.
- */
-Container.set({ id: ConnectionManager, type: ConnectionManager });
+import { TypeDIContainerProvider } from './container-provider.class';
 
 export * from './decorators/inject-connection.decorator';
 export * from './decorators/inject-manager.decorator';
 export * from './decorators/inject-repository.decorator';
+
+/**
+ * We need to set imported TypeORM classes before requesting them, otherwise we
+ * would receive a "ServiceNotFoundError" above TypeDI 0.9.1 from the decorators.
+ */
+TypeDIContainer.set({ id: ConnectionManager, type: ConnectionManager });
+
+/**
+ * We export the current container implementation what transforms function
+ * calls between the TypeORM container format and TypeDI.
+ */
+export const Container = new TypeDIContainerProvider();
