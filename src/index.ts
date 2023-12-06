4c7ba558679cbf311134b76f8e9ee9481b54f549
Gustavo commit <gustavo.dev.commit@gmail.com>
2023-12-06 10:26:35 -0300

    added export

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,10 @@ import { TypeDIContainerProvider } from './container-provider.class';
 export * from './decorators/inject-connection.decorator';
 export * from './decorators/inject-manager.decorator';
 export * from './decorators/inject-repository.decorator';
+export {useConnection} from './useConnection.class';
 
 import './container-registrations.const';
-
+import './connection-manager.class'
 /**
  * We export the current container implementation what transforms function
  * calls between the TypeORM container format and TypeDI.
